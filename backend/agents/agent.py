6c46a9c69b1d1ba03b1a165f462b36584a29055d
MessyCat <126219613+PERERALVV@users.noreply.github.com>
2024-01-29 12:32:57 +0000

    refactor formatter class and added new agents and modified some existing agents

--- backend/agents/agent.py
+++ backend/agents/agent.py
@@ -7,11 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 google_api_key = os.environ.get("google_api_key")
 
-
 class agent:
-    def __init__(self, template):
-        self.llm = ChatGoogleGenerativeAI(
-            model="gemini-pro", client="google", google_api_key=google_api_key)  # init the model
+    def __init__(self,template,temp=0.1,model="text"):
+        if model.lower() == "text":
+            self.model="gemini-pro"
+        elif model.lower() == "vision":
+            self.model = "gemini-pro-vision"
+        self.llm = ChatGoogleGenerativeAI(model=self.model, client="google", google_api_key= google_api_key ,temperature=temp) #init the model
         # create the prompt template
         self.template = template
         # create the prompt
@@ -20,56 +22,67 @@ class agent:
         self.output_parser = StrOutputParser()
 
     # use the model to make basic query
-    def query(self, input_text):
+    def query(self,input_text):
         response = self.llm.invoke(input_text)
         return response
 
-    # use the model to make structured query
-    def chainquery(self, input):
+    # use the model to make structred query
+    def chainquery(self,input):
         chain = self.prompt | self.llm | self.output_parser
         response = chain.invoke(input)
         return response
 
-
 class dev_agent(agent):
     def __init__(self):
 
-        # developer template
+        #developer template
         self.template = """
-        reply with the relevant html , css and js codes to create the following web page of the given project according to the given description:
+        reply only with the relevant html , css and js codes to create the following web page of the given project description according to the web page description:
+        
+        project name : {project_name}
+        project description:{project_description} and my project will contain the following web pages : {webpage_names}
+        webpage :{webpage}
+        web page description:{webpage_description}
+        
+        """
+        super().__init__(self.template,0.1,"text")
+        
+class code_checker(agent):
+    def __init__(self):
+        #developer template
+        self.template = """
+        analyze the given code and check for bugs and possible improvements and return the modified code:
+        *the codes should be complete and ready to run*
+        *reply only with the modified code*
+        
+        code:{code}
+        """
+        super().__init__(self.template,0.1,"text")
+
+class planner(agent):
+    def __init__(self):
+        #developer template
+        self.template = """
+        create a description for the given webpage of the given project:
+        
+        *describe the needed webpage asuming you are the client*
+        
         project:{project}
         webpage:{webpage}
-        description: {description}
         """
-        super().__init__(self.template)
+        super().__init__(self.template,0.1,"text")
 
 
-class formatter:
-    def __init__(self, input_string):
-        self.input_string = input_string
-        self.result_dict = {'html': '', 'css': '', 'js': ''}
-
-    def split_and_recognize(self):
-        if not self.input_string:
-            return self.result_dict
-
-        delimiter = "///"
-        parts = self.input_string.split(delimiter)
-
-        html_part = ""
-        css_part = ""
-        js_part = ""
-
-        for part in parts:
-            if "HTML" in part:
-                html_part = part.replace("HTML", "")
-            elif "CSS" in part:
-                css_part = part.replace("CSS", "")
-            elif "JS" in part:
-                js_part = part.replace("JS", "")
-
-        self.result_dict['html'] = html_part
-        self.result_dict['css'] = css_part
-        self.result_dict['js'] = js_part
-
-        return self.result_dict
+class dev_sup(agent):
+    def __init__(self):
+        #developer template
+        self.template = """
+        check if the given codes for the webpage of the given project is correct, complete and aligns with the given description of the webpage when the code is run:
+        
+        project name : {project_name}
+        project description:{project}
+        webpage:{webpage}
+        web page description:{description}
+        code:{code}
+        """
+        super().__init__(self.template,0.1,"text")
