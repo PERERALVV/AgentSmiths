ed6837a233c28f76a6a2ac11edb937e9c5d3cf84
ravindusenavirathna <ravindusasanka0514@gmail.com>
2024-01-31 17:45:04 +0530

    Remove unused code and class formatter

--- backend/agents/agent.py
+++ backend/agents/agent.py
@@ -91,34 +91,3 @@ class dev_sup(agent):
         code:{code}
         """
         super().__init__(self.template, 0.1, "text")
-
-
-class formatter:
-    def __init__(self, input_string):
-        self.input_string = input_string
-        self.result_dict = {'html': '', 'css': '', 'js': ''}
-
-    def split_and_recognize(self):
-        if not self.input_string:
-            return self.result_dict
-
-        delimiter = "```"
-        parts = self.input_string.split(delimiter)
-
-        html_part = ""
-        css_part = ""
-        js_part = ""
-
-        for part in parts:
-            if "HTML" in part:
-                html_part = part.replace("HTML", "")
-            elif "CSS" in part:
-                css_part = part.replace("CSS", "")
-            elif "JS" in part:
-                js_part = part.replace("JS", "")
-
-        self.result_dict['html'] = html_part
-        self.result_dict['css'] = css_part
-        self.result_dict['js'] = js_part
-
-        return self.result_dict
