7b5c0479adf204cc64acca3eaaaa01bd08303e2a
ravindusenavirathna <ravindusasanka0514@gmail.com>
2024-05-17 11:54:27 +0530

    font editor

--- frontend/src/components/pages/ShowOutput/FontEditor.jsx
+++ frontend/src/components/pages/ShowOutput/FontEditor.jsx
@@ -1,11 +1,38 @@
-import React from "react";
-import { GoogleFontLoader } from "react-google-fonts";
+import React, { useState, useEffect } from "react";
 
-const FontEditor = () => {
+const FontEditor = ({ onChange }) => {
+  const [fonts, setFonts] = useState([]);
+  const [selectedFont, setSelectedFont] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyB0g8CpYltBybAu1agE685z5bnFSZbePas"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setFonts(data.items.map((font) => font.family));
+      })
+      .catch((error) => console.error("Error fetching fonts:", error));
+  }, []);
+
+  const handleFontChange = (event) => {
+    const font = event.target.value;
+    setSelectedFont(font);
+    onChange(font);
+  };
+
   return (
-    <div style={{ backgroundColor: "blue" }}>
-      font editor
-      <GoogleFontLoader fonts={[{ font: "Roboto", weights: [400, 700] }]} />
+    <div>
+      {" "}
+      <select value={selectedFont} onChange={handleFontChange}>
+        <option value="">Select a font</option>
+        {fonts.map((font) => (
+          <option key={font} value={font} style={{ fontFamily: font }}>
+            {font}
+          </option>
+        ))}
+      </select>
+      <div style={{ fontFamily: selectedFont }}>{selectedFont}</div>
     </div>
   );
 };
